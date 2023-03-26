63c72201b44bda10aeb2e3d2fad26bdd746ad168
Yorick2002 <125297057+Yorick20022@users.noreply.github.com>
2023-03-27 01:10:34 +0200

    update function works properly now
    uwu

--- update-process.php
+++ update-process.php
@@ -8,10 +8,28 @@ $taak = $_POST["taak"];
 $prioriteit = $_POST["prio"];
 $deadline = $_POST["deadline"];
 $omschrijving = $_POST["omschrijving"];
-$categorie = $_GET["categorie"];
+$categorie = $_POST["categorie"];
 
 if (!empty($taak)) {
-    $stmt = $conn->prepare("UPDATE taken SET TaakNaam=?, Prioriteit=?, Deadline=?, Omschrijving=?, CategorieID=?  WHERE TaakID=?");
+    // Check if the category already exists in the categorie table
+    $stmt = $conn->prepare("SELECT CategorieID FROM categorie WHERE CategorieNaam = ?");
+    $stmt->execute([$categorie]);
+
+    if ($stmt->rowCount() > 0) {
+        // If the category already exists, get its existing CategorieID
+        $row = $stmt->fetch();
+        $categorieID = $row["CategorieID"];
+    } else {
+        // If the category doesn't exist, insert it into the categorie table
+        $stmt = $conn->prepare("INSERT INTO categorie (CategorieNaam) VALUES (?)");
+        $stmt->execute([$categorie]);
+
+        // Get the newly generated CategorieID value
+        $categorieID = $conn->lastInsertId();
+    }
+
+    // Update the taken table with the new values and generated CategorieID
+    $stmt = $conn->prepare("UPDATE taken SET TaakNaam=?, Prioriteit=?, Deadline=?, Omschrijving=?, CategorieID=? WHERE TaakID=?");
 
     if (!$stmt) {
         echo "Error" . $conn->errorInfo();
@@ -21,24 +39,15 @@ if (!empty($taak)) {
     $stmt->bindParam(2, $prioriteit);
     $stmt->bindParam(3, $deadline);
     $stmt->bindParam(4, $omschrijving);
-    $stmt->bindParam(5, $categorie);
+    $stmt->bindParam(5, $categorieID);
     $stmt->bindParam(6, $EditID);
     $stmt->execute();
-    
+
+
     if ($stmt->rowCount() > 0) {
         echo "Record succesvol bijgewerkt";
         echo "<br>";
-        echo $taak;
-        echo "<br>";
-        echo $prioriteit;
-        echo "<br>";
-        echo $deadline;
-        echo "<br>";
-        echo $omschrijving;
-        echo "<br>";
-        echo $categorie;
-        echo "<br>";
-        echo "id: $EditID";
+        var_dump($categorie);
 
 
     } else {
@@ -51,7 +60,7 @@ if (!empty($taak)) {
     echo "Error";
 }
 
-// header("Location: index.php");
-// exit;
+header("Location: index.php");
+exit;
 
 ?>
